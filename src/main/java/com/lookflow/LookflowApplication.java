e044adb5a7943ce378477f8ead25d9c993172656
eric <eric10soto@gmail.com>
2025-10-12 19:31:59 +0200

    Estructura del proyecto y de los packages creada para arquitectura hexagonal

---
+++ src/main/java/com/lookflow/LookflowApplication.java
@@ -0,0 +1,13 @@
+package com.lookflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lookflow/LookflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lookflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LookflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
